78cc67937c8c578c48e0310af964e5e71ea48271
s2lsoftener <s2@puyonexus.com>
2019-11-14 02:46:58 -0500

    refactored filter window implementation

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -1,5 +1,3 @@
-// import svgImage from '../img/testfilter.svg';
-const svgImage = chrome.extension.getURL(require('./../img/testfilter.svg'));
 const svgContent = `
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
   <defs>
@@ -8,201 +6,231 @@ const svgContent = `
     </filter>
   </defs>
 </svg>
-`
+`; 
 
-// Add a fixed div to the web page
-// const appWindow = document.createElement("div");
-// document.body.appendChild(appWindow);
-// appWindow.style.position = "fixed";
-// appWindow.style.width = "200px";
-// appWindow.style.height = "200px";
-// appWindow.style.top = "100px";
-// appWindow.style.left = "100px";
-// appWindow.style.zIndex = "999999999";
-// appWindow.style['background-color'] = "rgba(0, 0, 0, 0)";
-// appWindow.style.border = "3px solid red";
-// appWindow.style['backdrop-filter'] = "invert(0.8)";
-// appWindow.style['pointer-events'] = 'none';
+class FilterWindow {
+  /**
+   * @param {HTMLElement} target
+   */
+  constructor(target) {
+    // General settings
+    this.target = target;
+    this.defaultWidth = 100;
+    this.defaultHeight = 100;
+    this.cornerSize = 20;
+    this.lineThickness = 3;
+    this.x = 100;
+    this.y = 100;
 
-// Make the color change container
-// const ccContainer = document.createElement("div");
-// document.body.appendChild(ccContainer);
-// ccContainer.id = "CVExtension-Container";
-// ccContainer.style.position = "fixed";
-// ccContainer.style.width = "200px";
-// ccContainer.style.height = "200px";
-// ccContainer.style.top = "100px";
-// ccContainer.style.left = "100px";
-// ccContainer.style.zIndex = "999999999";
-// ccContainer.style['background-color'] = "rgba(0, 0, 0, 0)";
-// ccContainer.style.border = "3px solid red";
-// appWindow.style['backdrop-filter'] = "invert(0.8)";
-// ccContainer.style['pointer-events'] = 'none';
+    // Set objects
+    this.ccInner = document.createElement("div");
+    this.ccInner.innerHTML = svgContent;
+    this.target.appendChild(this.ccInner);
+    this.ccInner.style.position = "fixed";
+    this.ccInner.style.width = `${this.defaultWidth}px`;
+    this.ccInner.style.height = `${this.defaultHeight}px`;
+    this.ccInner.style.left = `${this.x}px`;
+    this.ccInner.style.top = `${this.y}px`;
+    this.ccInner.style.zIndex = "99999999";
+    this.ccInner.style['backdrop-filter'] = "url('#cvd')";
+    this.ccInner.style['pointer-events'] = 'none';
+    this.ccInner.style.transformOrigin = 'top left';
 
-// Make the inner part of the color change container.
-// This part can't be clicked?
+    // TopLeft Corner
+    this.ccTopLeft = document.createElement("div");
+    this.target.appendChild(this.ccTopLeft);
+    this.ccTopLeft.style.position = "fixed";
+    this.ccTopLeft.style.width = `${this.cornerSize}px`;
+    this.ccTopLeft.style.height = `${this.cornerSize}px`;
+    this.ccTopLeft.style.borderLeft = `${this.lineThickness}px solid red`;
+    this.ccTopLeft.style.borderTop = `${this.lineThickness}px solid red`;
+    this.ccTopLeft.style.boxSizing = "border-box";
+    this.ccTopLeft.style.left = `${this.x}px`;
+    this.ccTopLeft.style.top = `${this.y}px`;
+    this.ccTopLeft.style.zIndex = "99999999";
+    this.ccTopLeft.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    this.ccTopLeft.style.transformOrigin = 'top left';
 
-ccSettings = {
-  width: 200,
-  height: 200,
-  corner: 20,
-  thickness: 3,
-  x: 100,
-  y: 100,
-}
+    // TopRight Corner
+    this.ccTopRight = document.createElement("div");
+    this.target.appendChild(this.ccTopRight);
+    this.ccTopRight.style.position = "fixed";
+    this.ccTopRight.style.width = `${this.cornerSize}px`;
+    this.ccTopRight.style.height = `${this.cornerSize}px`;
+    this.ccTopRight.style.borderRight = `${this.lineThickness}px solid red`;
+    this.ccTopRight.style.borderTop = `${this.lineThickness}px solid red`;
+    this.ccTopRight.style.boxSizing = "border-box";
+    this.ccTopRight.style.left = `${this.x + this.defaultWidth - this.cornerSize}px`;
+    this.ccTopRight.style.top = `${this.y}px`;
+    this.ccTopRight.style.zIndex = "99999999";
+    this.ccTopRight.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    this.ccTopRight.style.transformOrigin = 'top right';
 
-console.log(ccSettings);
-console.log(svgImage);
-console.log(typeof svgImage);
+    this.ccBottomLeft = document.createElement("div");
+    this.target.appendChild(this.ccBottomLeft);
+    this.ccBottomLeft.style.position = "fixed";
+    this.ccBottomLeft.style.width = `${this.cornerSize}px`;
+    this.ccBottomLeft.style.height = `${this.cornerSize}px`;
+    this.ccBottomLeft.style.borderLeft = `${this.lineThickness}px solid red`;
+    this.ccBottomLeft.style.borderBottom = `${this.lineThickness}px solid red`;
+    this.ccBottomLeft.style.boxSizing = "border-box";
+    this.ccBottomLeft.style.left = `${this.x}px`;
+    this.ccBottomLeft.style.top = `${this.y + this.defaultHeight - this.cornerSize}px`;
+    this.ccBottomLeft.style.zIndex = "99999999";
+    this.ccBottomLeft.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    this.ccBottomLeft.style.transformOrigin = 'bottom left';
 
-// Inner
-const ccInner = document.createElement("div");
-ccInner.innerHTML = svgContent;
-document.body.appendChild(ccInner);
-ccInner.id = "ccInner";
-ccInner.style.position = "fixed";
-ccInner.style.width = `${ccSettings.width}px`;
-ccInner.style.height = `${ccSettings.height}px`;
-ccInner.style.left = `${ccSettings.x}px`;
-ccInner.style.top = `${ccSettings.y}px`;
-ccInner.style.zIndex = "99999999";
-// ccInner.style['backdrop-filter'] = `url("${svgImage}#myFilter")`
-// ccInner.style['backdrop-filter'] = 'invert(0.8)';
-ccInner.style['backdrop-filter'] = "url('#cvd')";
-ccInner.style['pointer-events'] = 'none';
+    this.ccBottomRight = document.createElement("div");
+    this.target.appendChild(this.ccBottomRight);
+    this.ccBottomRight.style.position = "fixed";
+    this.ccBottomRight.style.width = `${this.cornerSize}px`;
+    this.ccBottomRight.style.height = `${this.cornerSize}px`;
+    this.ccBottomRight.style.borderRight = `${this.lineThickness}px solid red`;
+    this.ccBottomRight.style.borderBottom = `${this.lineThickness}px solid red`;
+    this.ccBottomRight.style.boxSizing = "border-box";
+    this.ccBottomRight.style.left = `${this.x + this.defaultWidth - this.cornerSize}px`;
+    this.ccBottomRight.style.top = `${this.y + this.defaultHeight - this.cornerSize}px`;
+    this.ccBottomRight.style.zIndex = "99999999";
+    this.ccBottomRight.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    this.ccBottomRight.style.transformOrigin = 'bottom right';
 
-// TopLeft Corner
-const ccTopLeft = document.createElement("div");
-document.body.appendChild(ccTopLeft);
-ccTopLeft.id = "ccTopLeft";
-ccTopLeft.style.position = "fixed";
-ccTopLeft.style.width = `${ccSettings.corner}px`;
-ccTopLeft.style.height = `${ccSettings.corner}px`;
-ccTopLeft.style.borderLeft = `${ccSettings.thickness}px solid red`;
-ccTopLeft.style.borderTop = `${ccSettings.thickness}px solid red`;
-ccTopLeft.style.boxSizing = "border-box";
-ccTopLeft.style.left = `${ccSettings.x}px`;
-ccTopLeft.style.top = `${ccSettings.y}px`;
-ccTopLeft.style.zIndex = "99999999";
-ccTopLeft.style.backgroundColor = "rgba(0, 0, 0, 0)";
-
-// TopRight Corner
-const ccTopRight = document.createElement("div");
-document.body.appendChild(ccTopRight);
-ccTopRight.id = "ccTopRight";
-ccTopRight.style.position = "fixed";
-ccTopRight.style.width = `${ccSettings.corner}px`;
-ccTopRight.style.height = `${ccSettings.corner}px`;
-ccTopRight.style.borderRight = `${ccSettings.thickness}px solid red`;
-ccTopRight.style.borderTop = `${ccSettings.thickness}px solid red`;
-ccTopRight.style.boxSizing = "border-box";
-ccTopRight.style.left = `${ccSettings.x + ccSettings.width - ccSettings.corner}px`;
-ccTopRight.style.top = `${ccSettings.y}px`;
-ccTopRight.style.zIndex = "99999999";
-ccTopRight.style.backgroundColor = "rgba(0, 0, 0, 0)";
-
-// BottomLeft corner
-const ccBottomLeft = document.createElement("div");
-document.body.appendChild(ccBottomLeft);
-ccBottomLeft.id = "ccBottomLeft";
-ccBottomLeft.style.position = "fixed";
-ccBottomLeft.style.width = `${ccSettings.corner}px`;
-ccBottomLeft.style.height = `${ccSettings.corner}px`;
-ccBottomLeft.style.borderLeft = `${ccSettings.thickness}px solid red`;
-ccBottomLeft.style.borderBottom = `${ccSettings.thickness}px solid red`;
-ccBottomLeft.style.boxSizing = "border-box";
-ccBottomLeft.style.left = `${ccSettings.x}px`;
-ccBottomLeft.style.top = `${ccSettings.y + ccSettings.height - ccSettings.corner}px`;
-ccBottomLeft.style.zIndex = "99999999";
-ccBottomLeft.style.backgroundColor = "rgba(0, 0, 0, 0)";
-
-// BottomRight corner 
-const ccBottomRight = document.createElement("div");
-document.body.appendChild(ccBottomRight);
-ccBottomRight.id = "ccBottomRight";
-ccBottomRight.style.position = "fixed";
-ccBottomRight.style.width = `${ccSettings.corner}px`;
-ccBottomRight.style.height = `${ccSettings.corner}px`;
-ccBottomRight.style.borderRight = `${ccSettings.thickness}px solid red`;
-ccBottomRight.style.borderBottom = `${ccSettings.thickness}px solid red`;
-ccBottomRight.style.boxSizing = "border-box";
-ccBottomRight.style.left = `${ccSettings.x + ccSettings.width - ccSettings.corner}px`;
-ccBottomRight.style.top = `${ccSettings.y + ccSettings.height - ccSettings.corner}px`;
-ccBottomRight.style.zIndex = "99999999";
-ccBottomRight.style.backgroundColor = "rgba(0, 0, 0, 0)";
-
-enableDrag(ccTopLeft, "TopLeft");
-enableDrag(ccTopRight, "TopRight");
-enableDrag(ccBottomLeft, "BottomLeft");
-enableDrag(ccBottomRight, "BottomRight");
-
-function enableDrag(element, corner) {
-  let startX = element.offsetLeft;
-  let startY = element.offsetTop;
-  let offsetX = 0;
-  let offsetY = 0;
-
-  element.onmousedown = dragMousedown;
-
-  function dragMousedown(e) {
-    e = e || window.event;
-    e.preventDefault();
-  
-    // Get the mouse cursor position at startup
-    startX = e.clientX;
-    startY = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
+    // Enable drag
+    this.enableDrag(this.ccTopLeft, "TopLeft");
+    this.enableDrag(this.ccTopRight, "TopRight");
+    this.enableDrag(this.ccBottomLeft, "BottomLeft");
+    this.enableDrag(this.ccBottomRight, "BottomRight");
   }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
+  /**
+   * 
+   * @param {HTMLElement} element 
+   * @param {("TopLeft"|"TopRight"|"BottomLeft"|"BottomRight")} corner 
+   */
+  enableDrag(element, corner) {
+    const self = this;
+    
+    element.onmousedown = mouseDown;
 
-    // calculate the new cursor position
-    offsetX = startX - e.clientX;
-    offsetY = startY - e.clientY;
-    startX = e.clientX;
-    startY = e.clientY;
+    let offsetX = 0;
+    let offsetY = 0;
 
-    // Set the element's new position:
-    element.style.top = (element.offsetTop - offsetY) + "px";
-    element.style.left = (element.offsetLeft - offsetX) + "px";
+    /**
+     * @param {MouseEvent} e 
+     */
+    function mouseDown(e) {
+      e.preventDefault();
+      offsetX = e.clientX - element.offsetLeft;
+      offsetY = e.clientY - element.offsetTop;
 
-    // Calculate the new size of the app window
-    if (corner === "TopLeft") {
-      ccBottomLeft.style.left = (element.offsetLeft - offsetX) + "px";
-      ccTopRight.style.top = (element.offsetTop - offsetY) + "px";
-      ccInner.style.left = (element.offsetLeft - offsetX) + "px";
-      ccInner.style.top = (element.offsetTop - offsetY) + "px";
-      ccInner.style.width = ccInner.offsetWidth + offsetX + "px";
-      ccInner.style.height = ccInner.offsetHeight + offsetY + "px";
-    } else if (corner === "TopRight") {
-      ccBottomRight.style.left = (element.offsetLeft - offsetX) + "px";
-      ccTopLeft.style.top = (element.offsetTop - offsetY) + "px";
-      ccInner.style.left = (element.offsetLeft - offsetX - ccInner.offsetWidth + ccSettings.corner) + "px";
-      ccInner.style.top = (element.offsetTop - offsetY) + "px";
-      ccInner.style.width = ccInner.offsetWidth - offsetX + "px";
-      ccInner.style.height = ccInner.offsetHeight + offsetY + "px";
-    } else if (corner === "BottomLeft") {
-      ccTopLeft.style.left = (element.offsetLeft - offsetX) + "px";
-      ccBottomRight.style.top = (element.offsetTop - offsetY) + "px";
-      ccInner.style.left = (element.offsetLeft - offsetX) + "px";
-      ccInner.style.top = (element.offsetTop - offsetY - ccInner.offsetHeight + ccSettings.corner) + "px";
-      ccInner.style.width = ccInner.offsetWidth + offsetX + "px";
-      ccInner.style.height = ccInner.offsetHeight - offsetY + "px";
-    } else if (corner === "BottomRight") {
-      ccTopRight.style.left = (element.offsetLeft - offsetX) + "px";
-      ccBottomLeft.style.top = (element.offsetTop - offsetY) + "px";
-      ccInner.style.left = (element.offsetLeft - offsetX - ccInner.offsetWidth + ccSettings.corner) + "px";
-      ccInner.style.top = (element.offsetTop - offsetY - ccInner.offsetHeight + ccSettings.corner) + "px";
-      ccInner.style.width = ccInner.offsetWidth - offsetX + "px";
-      ccInner.style.height = ccInner.offsetHeight - offsetY + "px";
+      document.onmouseup = mouseUp;
+      document.onmousemove = mouseMove;
+    }
+
+    /**
+     * @param {MouseEvent} e 
+     */
+    function mouseMove(e) {
+      e.preventDefault();
+
+      // Set the element's new position
+      element.style.left = `${e.clientX - offsetX}px`;
+      element.style.top = `${e.clientY - offsetY}px`;
+
+      // Calculate how the other buttons should move.
+      if (corner === "TopLeft") {
+        self.ccBottomLeft.style.left = `${e.clientX - offsetX}px`;
+        self.ccTopRight.style.top = `${e.clientY - offsetY}px`;
+      } else if (corner === "TopRight") {
+        self.ccBottomRight.style.left = `${e.clientX - offsetX}px`;
+        self.ccTopLeft.style.top = `${e.clientY - offsetY}px`;
+      } else if (corner === "BottomLeft") {
+        self.ccTopLeft.style.left = `${e.clientX - offsetX}px`;
+        self.ccBottomRight.style.top = `${e.clientY - offsetY}px`;
+      } else if (corner === "BottomRight") {
+        self.ccTopRight.style.left = `${e.clientX - offsetX}px`;
+        self.ccBottomLeft.style.top = `${e.clientY - offsetY}px`;
+      }
+
+      const width = self.ccTopRight.offsetLeft - self.ccTopLeft.offsetLeft + self.cornerSize;
+      const height = self.ccBottomLeft.offsetTop - self.ccTopLeft.offsetTop + self.cornerSize;
+      self.ccInner.style.width = `${Math.abs(width)}px`;
+      self.ccInner.style.height = `${Math.abs(height)}px`;
+      self.ccInner.style.left = `${self.ccTopLeft.offsetLeft}px`;
+      self.ccInner.style.top = `${self.ccTopLeft.offsetTop}px`;
+
+      if (self.ccTopLeft.offsetLeft <= self.ccTopRight.offsetLeft + self.cornerSize && self.ccTopLeft.offsetTop <= self.ccBottomLeft.offsetTop + self.cornerSize) {
+        self.ccInner.style.transform = 'scaleX(1) scaleY(1)';
+        self.ccTopLeft.style.transform = 'scaleX(1) scaleY(1)';
+        self.ccTopRight.style.transform = 'scaleX(1) scaleY(1)';
+        self.ccBottomLeft.style.transform = 'scaleX(1) scaleY(1)';
+        self.ccBottomRight.style.transform = 'scaleX(1) scaleY(1)';
+      } else if (self.ccTopLeft.offsetLeft > self.ccTopRight.offsetLeft + self.cornerSize && self.ccTopLeft.offsetTop <= self.ccBottomLeft.offsetTop + self.cornerSize) {
+        self.ccInner.style.transform = 'scaleX(-1) scaleY(1)';
+        self.ccTopLeft.style.transform = 'scaleX(-1) scaleY(1)';
+        self.ccTopRight.style.transform = 'scaleX(-1) scaleY(1)';
+        self.ccBottomLeft.style.transform = 'scaleX(-1) scaleY(1)';
+        self.ccBottomRight.style.transform = 'scaleX(-1) scaleY(1)';
+      } else if (self.ccTopLeft.offsetLeft <= self.ccTopRight.offsetLeft + self.cornerSize && self.ccTopLeft.offsetTop > self.ccBottomLeft.offsetTop + self.cornerSize) {
+        self.ccInner.style.transform = 'scaleX(1) scaleY(-1)'
+        self.ccTopLeft.style.transform = 'scaleX(1) scaleY(-1)';
+        self.ccTopRight.style.transform = 'scaleX(1) scaleY(-1)';
+        self.ccBottomLeft.style.transform = 'scaleX(1) scaleY(-1)';
+        self.ccBottomRight.style.transform = 'scaleX(1) scaleY(-1)';
+      } else if (self.ccTopLeft.offsetLeft > self.ccTopRight.offsetLeft + self.cornerSize && self.ccTopLeft.offsetTop > self.ccBottomLeft.offsetTop + self.cornerSize) {
+        self.ccInner.style.transform = 'scaleX(-1) scaleY(-1)'
+        self.ccTopLeft.style.transform = 'scaleX(-1) scaleY(-1)';
+        self.ccTopRight.style.transform = 'scaleX(-1) scaleY(-1)';
+        self.ccBottomLeft.style.transform = 'scaleX(-1) scaleY(-1)';
+        self.ccBottomRight.style.transform = 'scaleX(-1) scaleY(-1)';
+      }
+    }
+
+    /**
+     * @param {MouseEvent} e
+     */
+    function mouseUp(e) {
+      e.preventDefault(e);
+      document.onmouseup = null;
+      document.onmousemove = null;
     }
   }
 
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
+  /**
+   * @param {string} filter
+   */
+  updateSVGFilter(filter) {
+    this.ccInner.innerHTML = `
+    	<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+    		<defs>
+      		<filter x="0" y="0" width="99999" height="99999" id="cvd">
+        		${filter}
+      		</filter>
+    		</defs>
+  		</svg>
+    `
   }
 }
+
+/**
+ * @returns {FilterWindow[]}
+ */
+function getFilterWindows() {
+  // Set filterWindows global variable if not found
+  if (window.filterWindows === undefined) {
+    window.filterWindows = [new FilterWindow(document.body)];
+    return window.filterWindows;
+  }
+  // Otherwise, just return the variable
+  else {
+    return window.filterWindows;
+  }
+}
+
+// Page messaging
+// https://developer.chrome.com/extensions/messaging#connect
+chrome.runtime.onConnect.addListener(function(port) {
+  port.onMessage.addListener(function(msg) {
+    const windows = getFilterWindows();
+    console.log(windows);
+  });
+});
--- src/js/options.js
+++ src/js/options.js
@@ -4,8 +4,17 @@ let changeColor = document.getElementById('apply-color-change');
 
 changeColor.onclick = function(element) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    console.log("Clicked the button");
+    // Inject content script into current tab
     chrome.tabs.executeScript(tabs[0].id, {
       file: 'contentScript.bundle.js'
+    }, () => {
+      const port = chrome.tabs.connect(tabs[0].id);
+      // port.onMessage.addListener(response => {
+      //   console.log(response);
+      // });
+      port.postMessage({ name: 'cvdExtension'});
     });
   });
 };
+
